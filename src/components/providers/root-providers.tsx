a97ae82936b78a38e56131356b6e6ec4cc42407f
Julian Curalli <julian.curalli27@gmail.com>
2026-07-14 22:31:20 -0400

    another import fix

--- src/components/providers/root-providers.tsx
+++ src/components/providers/root-providers.tsx
@@ -1,5 +1,5 @@
 import { ClerkProvider } from "@clerk/nextjs";
-import { ConvexClientProvider } from "./ConvexClientProvider";
+import { ConvexClientProvider } from "./convex-client-provider";
 
 export function RootProviders({ children }: { children: React.ReactNode }) {
   return (
